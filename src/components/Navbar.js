62ce01c18bf4ae7dad05e946fbbe2b2ac861a615
unknown <guptagouri89@gmail.com>
2025-08-05 06:45:13 +0530

    added login component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,8 +35,8 @@ const Navbar = () => {
             </li>
           </ul>
           <form className="d-flex">
-             <Link className="btn btn-primary mx-2" to="/login" role="button">Login</Link>
-             <Link className="btn btn-primary mx-2" to="/signup" role="button">Signup</Link>
+             <Link className="btn btn-primary mx-1" to="/login" role="button">Login</Link>
+             <Link className="btn btn-primary mx-1" to="/signup" role="button">Signup</Link>
           </form>
         </div>
       </div>
